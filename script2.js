8d489f0654768953675c8d187142c9e0794ad1f7
oslek222 <87049204+oslek222@users.noreply.github.com>
2021-07-29 20:05:39 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+//loops
+var tmp="";
+//for loop
+for(var i=0;i<=10;i++){//i+=1 i++
+tmp +=`<p class="bordered">Description ${i}</
+p>`;
+console.log(tmp);
+}
---
+++ script4.js
@@ -0,0 +1,19 @@
+let band="metalica", 
+song="enter sandman",
+text;
+
+text=band+ " : " +song;
+text=text.concat(" ","it's great!")
+console.log(text.lenght);
+console.log(text.concat.toLowerCase());
+console.log(text.concat.toUpperCase());
+
+let activities= "i am learning javascript";
+text=activity.slpit("");
+let hobbies="play videogames, read, watch tv, listen to music";
+text=hobbies.split(",");
+console.log(text);
+
+let email="matt@gmail.com";
+text=email.includes("@gmail.com");
+console.log(text);
